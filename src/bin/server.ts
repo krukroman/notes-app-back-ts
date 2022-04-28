af9e0cf7783fc4aaa12498b1454b6dd9b86dee72
Roman Kruk <wilmlass@gmail.com>
2022-04-28 20:06:30 +0300

    corrections, created docker-compose file

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -3,5 +3,5 @@ const app = require('../app');
 const { PORT = 3000 } = process.env;
 
 app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
+  console.log(`Server running on address: http://localhost:${PORT}`);
 });
